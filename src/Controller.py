d01c02b3f8898fdfe6af0320dc4e714b2bdd43ac
ulrikisdahl <uisdahl@gmail.com>
2025-01-18 17:16:43 +0100

    Add neural network controller

--- src/Controller.py
+++ src/Controller.py
@@ -8,15 +8,16 @@ class BaseController:
 
     
 class PIDController(BaseController):
-    def __init__(self, cfg: dict):
+    def __init__(self, cfg: dict, key: jax.random.PRNGKey):
         self.learning_rate = cfg["learning_rate"]
-        self.key = jax.random.key(cfg["seed"])
-        self.k_p = jax.random.uniform(self.key, shape=(1))
-        self.k_i = jax.random.uniform(self.key, shape=(1))
-        self.k_d = jax.random.uniform(self.key, shape=(1))
+        # self.key = jax.random.key(cfg["seed"])
+        self.k_p = jax.random.uniform(key, shape=(1))
+        self.k_i = jax.random.uniform(key, shape=(1))
+        self.k_d = jax.random.uniform(key, shape=(1))
         self.error_history = 0.0
     
-    def __call__(self, params: jax.Array, error: float, d_error: float, error_history: float):
+    @staticmethod
+    def __call__(params: jax.Array, error: float, d_error: float, error_history: float):
         """
         Computes the control signal based on the error
         Args:
@@ -31,15 +32,12 @@ class PIDController(BaseController):
         """
         Updates the controller parameters based on the gradients
         """
-        self.k_p = params[0] - self.learning_rate * gradients[0]  
+        self.k_p = params[0] - self.learning_rate * gradients[0]
         self.k_i = params[1] - self.learning_rate * gradients[1]
         self.k_d = params[2] - self.learning_rate * gradients[2]
 
     def update_error(self, error: float):
         self.error_history += error
-
-    # def update_params(self, params: jax.Array):
-    #     self.k_p, self.k_i, self.k_d = params
 
     def get_params(self):
         return jnp.array([self.k_p, self.k_i, self.k_d])
@@ -52,3 +50,51 @@ class PIDController(BaseController):
 
     def reset(self): #TODO: Could be move to a base class
         self.error_history = 0.0
+
+
+class NeuralNetworkController(BaseController):
+    def __init__(self, cfg: dict, key: jax.random.PRNGKey):
+        self.input_dim = 3
+        self.output_dim = 1 
+        self.key = key
+        self.learning_rate = cfg["learning_rate"]
+        self.num_layers = cfg["num_layers"]
+        self.num_neurons = cfg["num_neurons"]
+        self.activation_fn = cfg["activation_fn"]
+        self.weight_min = cfg["weight_range_low"]
+        self.weight_max = cfg["weight_range_high"] 
+        self.weights = None
+        self._init_weights()
+
+
+    def __call__(self, params: jax.Array, error: float, d_error: float, error_history: float):
+        """
+        x*W + b
+        """
+        # num_layers = params["num_layers"] #TODO: use object state variables instead
+        # weights = params["weights"]
+        weights = params
+        activation = jnp.array([error, d_error, error_history, 1.0]) #TODO: terrible naming
+        for layer in range(self.num_layers + 1): 
+            layer_output = jnp.matmul(activation, weights[layer])
+            if layer < self.num_layers:
+                layer_output = jnp.append(layer_output, 1.0) #bias trick            
+                activation = jnp.maximum(layer_output, 0) #TODO: Generalize 
+        
+        return layer_output #TODO: Add output function (then you wont need to clamp current_level)
+    
+    def _init_weights(self):
+        weights = []
+        in_dims = [self.input_dim] + [self.num_neurons for _ in range(self.num_layers)] 
+        out_dims = [self.num_neurons for _ in range(self.num_layers)] + [self.output_dim] 
+        for layer in range(self.num_layers + 1):
+            weight_param = jax.random.uniform(self.key, shape=(in_dims[layer] + 1, out_dims[layer]), minval=self.weight_min, maxval=self.weight_max) # +1 for bias trick
+            weights.append(weight_param)
+        self.weights = weights
+
+    def get_params(self):
+        return self.weights
+
+    def update_params(self, params: jax.Array, gradients: jax.Array):
+        for layer in range(self.num_layers + 1):        
+            self.weights[layer] = params[layer] - self.learning_rate * gradients[layer]
--- src/System.py
+++ src/System.py
@@ -1,4 +1,4 @@
-from utils import get_class
+from utils import get_class, plot_mse_per_epoch
 import jax.numpy as jnp
 import jax
 from tqdm import tqdm
@@ -7,32 +7,32 @@ from src.Controller import BaseController
 
     
 def step_fn(
-        control_signal: float,
-        plant: BasePlant,
-        plant_state: dict,
-        disturbance: float
-    ):
+    control_signal: float,
+    plant: BasePlant,
+    plant_state: dict,
+    disturbance: float
+):
     """
     Outputs error and is used to compute derivative w.r.t. controller output
     """
     new_level, target_level = plant.evaluate(disturbance, control_signal, plant_state)
     error = target_level - new_level
-    return (error[0], new_level)  #NOTE: error[0]
+    return (error, new_level)  #NOTE: error[0]
 
 #@jax.jit
 def epoch_fn(
-        key: jax.random.PRNGKey,
-        epoch_nr: int,
-        noise_range: tuple[float, float], 
-        num_steps: int,
-        parameters: jax.Array,  
-        plant: BasePlant,
-        controller: BaseController
-    ):
+    key: jax.random.PRNGKey,
+    epoch_nr: int,
+    noise_range: tuple[float, float], 
+    num_steps: int,
+    parameters: jax.Array,  
+    plant: BasePlant,
+    controller: BaseController
+):
     """
     Function that outputs MSE and we compute derivative w.r.t. controller parameters
     """
-    plant.reset() 
+    plant.reset() #NOTE: How did this work??????
     error = 0.0
     d_error = 0.0
     error_history = 0.0 #TODO: Move to inside controller
@@ -45,9 +45,10 @@ def epoch_fn(
     error_grad_fn = jax.value_and_grad(step_fn, argnums=0, has_aux=True) #derivative of error w.r.t. control_signal 
     for step in range(num_steps):
         #forward 
-        plant_state = plant.get_state() #NOTE: This gives state variables from Plant - might have to "grab them" before we call epoch_fn (or the whole object is a epoch_fn argument so it is pure)
+        # print(f"STEP: {step}")
+        plant_state = plant.get_state() 
         control_signal = controller(parameters, error, d_error, error_history)
-        (error, new_level), d_error = error_grad_fn(control_signal, plant, plant_state, disturbances[step])
+        (error, new_level), d_error = error_grad_fn(control_signal[0], plant, plant_state, disturbances[step])
         
         #update stuff
         error_history += error
@@ -72,20 +73,24 @@ class Consys:
         plant_class = get_class("src.Plant", cfg["plant"]["class"])
         self.plant = plant_class(cfg["plant"])
         controller_class = get_class("src.Controller", cfg["controller"]["class"])
-        self.controller = controller_class(cfg["controller"])
-
+        self.controller = controller_class(cfg["controller"], self.key)
 
     def run(self):
         """
         """
-        grad_fn = jax.grad(epoch_fn, argnums=4) #derivative w.r.t. controller_params
+        mse_log = []
+        grad_fn = jax.value_and_grad(epoch_fn, argnums=4) #derivative w.r.t. controller_params
         for epoch in tqdm(range(self.epochs)):
             controller_parameters = self.controller.get_params()
-            mse_grad = grad_fn(
+            value, mse_grad = grad_fn(
                 self.key, epoch, (self.noise_range_low, self.noise_range_high), self.num_timesteps, controller_parameters, self.plant, self.controller
             )
             self.controller.update_params(controller_parameters, mse_grad)
-            
+            print(f"Epoch {epoch}: {value}")
+            mse_log.append(value)
+
+        
+        plot_mse_per_epoch(mse_log)    
         print("DONE")
 
             
